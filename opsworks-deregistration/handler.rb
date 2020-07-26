040429ae6abbbcce1a5c033e6bd393d20dbfe866
Jay Bonci <jay@bonci.net>
2020-07-25 22:31:59 -0400

    Update to the handler for working code

--- opsworks-deregistration/handler.rb
+++ opsworks-deregistration/handler.rb
@@ -19,18 +19,22 @@ def lambda_handler(args)
     return http_response(400, "No event to parse")
   end
 
+  pp event['Records'][0]
+
   message = JSON.parse(event['Records'][0]['Sns']['Message'])
+  pp message
+
   if message['Event'].eql? 'autoscaling:EC2_INSTANCE_TERMINATE'
-    ec2client = Aws::EC2::Client.new(ENV['AWS_DEFAULT_REGION'])
+    ec2client = Aws::EC2::Client.new(region: ENV['AWS_DEFAULT_REGION'])
     
     instance_id = message['EC2InstanceId']
     puts "Removing instance from OpsWorks: #{instance_id}"
 
-    ec2client.describe_instances(instance_ids: [instance_id]).reservations[0].tags.each do |tag|
+    ec2client.describe_instances(instance_ids: [instance_id]).reservations[0].instances[0].tags.each do |tag|
       if tag.key.eql? 'opsworks_stack_id'
         stack_id = tag.value
         puts "Checking OpsWorks Stack: #{stack_id}"
-        opsworks_client = AWS::OpsWorks::Client.new(ENV['AWS_DEFAULT_REGION'])
+        opsworks_client = AWS::OpsWorks::Client.new(region: ENV['AWS_DEFAULT_REGION'])
         opsworks_client.describe_instances(stack_ids: [stack_id]).instances.each do |instance|
           if instance.ec2_instance_id.eql? instance_id
             puts "Instance #{instance_id} found in stack: #{stack_id}, deregistering"
